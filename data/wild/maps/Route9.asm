5fcb3d372f8d672066b4f56f138cd0372972e466
ClutchThompson <157330753+ClutchThompson@users.noreply.github.com>
2026-05-15 18:44:18 -0400

    Replace RATTATA and SPEAROW with SANDSHREW in Route9

--- data/wild/maps/Route9.asm
+++ data/wild/maps/Route9.asm
@@ -2,14 +2,14 @@ Route9WildMons:
 	def_grass_wildmons 15 ; encounter rate
 	db 16, RATTATA
 	db 16, SPEAROW
-	db 14, RATTATA
+	db 14, SANDSHREW
 IF DEF(_RED)
 	db 11, EKANS
 	db 13, SPEAROW
 	db 15, EKANS
 	db 17, RATTATA
-	db 17, SPEAROW
-	db 13, EKANS
+	db 17, SANDSHREW
+	db 13, SANDSHREW
 	db 17, EKANS
 ENDC
 IF DEF(_BLUE)
