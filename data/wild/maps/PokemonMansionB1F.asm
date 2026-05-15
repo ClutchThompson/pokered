4fec2203a4381a6a07edd3fe01fe8051516597ec
ClutchThompson <157330753+ClutchThompson@users.noreply.github.com>
2026-05-15 18:49:01 -0400

    Replace KOFFING with GRIMER and update MAGMAR

--- data/wild/maps/PokemonMansionB1F.asm
+++ data/wild/maps/PokemonMansionB1F.asm
@@ -2,13 +2,13 @@ PokemonMansionB1FWildMons:
 	def_grass_wildmons 10 ; encounter rate
 IF DEF(_RED)
 	db 33, KOFFING
-	db 31, KOFFING
+	db 31, GRIMER
 	db 35, GROWLITHE
 	db 32, PONYTA
-	db 31, KOFFING
-	db 40, WEEZING
-	db 34, PONYTA
-	db 35, GRIMER
+	db 31, MAGMAR
+	db 40, MAGMAR
+	db 34, MAGMAR
+	db 35, MAGMAR
 	db 42, WEEZING
 	db 42, MUK
 ENDC
