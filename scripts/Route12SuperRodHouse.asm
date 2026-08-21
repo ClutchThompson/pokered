5f105a64741f7318746c00bd704f2237b1cf88dd
ClutchThompson <157330753+ClutchThompson@users.noreply.github.com>
2026-08-21 13:01:12 -0400

    Rename Route12SuperRodHouse to FuchsiaGoodRodHouse

--- scripts/Route12SuperRodHouse.asm
+++ scripts/Route12SuperRodHouse.asm
@@ -1,58 +1,67 @@
-Route12SuperRodHouse_Script:
+FuchsiaGoodRodHouse_Script:
 	jp EnableAutoTextBoxDrawing
 
-Route12SuperRodHouse_TextPointers:
+FuchsiaGoodRodHouse_TextPointers:
 	def_text_pointers
-	dw_const Route12SuperRodHouseFishingGuruText, TEXT_ROUTE12SUPERRODHOUSE_FISHING_GURU
+	dw_const FuchsiaGoodRodHouseFishingGuruText, TEXT_FUCHSIAGOODRODHOUSE_FISHING_GURU
 
-Route12SuperRodHouseFishingGuruText:
+FuchsiaGoodRodHouseFishingGuruText:
 	text_asm
 	ld a, [wStatusFlags1]
-	bit BIT_GOT_SUPER_ROD, a
+	bit BIT_GOT_GOOD_ROD, a
 	jr nz, .got_item
-	ld hl, .DoYouLikeToFishText
+	ld hl, .Text
 	call PrintText
 	call YesNoChoice
 	ld a, [wCurrentMenuItem]
 	and a
 	jr nz, .refused
-	lb bc, SUPER_ROD, 1
+	lb bc, GOOD_ROD, 1
 	call GiveItem
 	jr nc, .bag_full
 	ld hl, wStatusFlags1
-	set BIT_GOT_SUPER_ROD, [hl]
-	ld hl, .ReceivedSuperRodText
+	set BIT_GOT_GOOD_ROD, [hl]
+	ld hl, .ReceivedGoodRodText
 	jr .done
 .bag_full
 	ld hl, .NoRoomText
 	jr .done
 .refused
-	ld hl, .ThatsDisappointingText
+	ld hl, .ThatsSoDisappointingText
 	jr .done
 .got_item
-	ld hl, .TryFishingText
+	ld hl, .HowAreTheFishText
 .done
 	call PrintText
 	jp TextScriptEnd
 
-.DoYouLikeToFishText:
-	text_far _Route12SuperRodHouseFishingGuruDoYouLikeToFishText
+.Text:
+	text_far _FuchsiaGoodRodHouseFishingGuruText
 	text_end
 
-.ReceivedSuperRodText:
-	text_far _Route12SuperRodHouseFishingGuruReceivedSuperRodText
+.ReceivedGoodRodText:
+	text_far _FuchsiaGoodRodHouseFishingGuruReceivedGoodRodText
 	sound_get_item_1
-	text_far _Route12SuperRodHouseFishingGuruFishingWayOfLifeText
 	text_end
 
-.ThatsDisappointingText:
-	text_far _Route12SuperRodHouseFishingGuruThatsDisappointingText
+.UnusedText:
+	para "つり　こそ"
+	line "おとこの　ロマン　だ！"
+
+	para "へぼいつりざおは"
+	line "コイキングしか　つれ　なんだが"
+	line "この　いいつりざおなら"
+	line "もっと　いいもんが　つれるんじゃ！"
+	done
+
+.ThatsSoDisappointingText:
+	text_far _FuchsiaGoodRodHouseFishingGuruThatsSoDisappointingText
 	text_end
 
-.TryFishingText:
-	text_far _Route12SuperRodHouseFishingGuruTryFishingText
+.HowAreTheFishText:
+	text_far _FuchsiaGoodRodHouseFishingGuruHowAreTheFishText
 	text_end
 
 .NoRoomText:
-	text_far _Route12SuperRodHouseFishingGuruNoRoomText
+	text_far _FuchsiaGoodRodHouseFishingGuruNoRoomText
 	text_end
